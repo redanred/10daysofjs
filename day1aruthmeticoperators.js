b144f6070a1dbdc55100490ab3f13db29fd02db5
redanred <102037305+redanred@users.noreply.github.com>
2022-09-11 16:22:52 -0300

    Add files via upload

---
+++ day1aruthmeticoperators.js
@@ -0,0 +1,63 @@
+/* Task
+
+Complete the following functions in the editor below:
+
+getArea(length, width): Calculate and return the area of a rectangle having sides  and .
+getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides  and .
+The values returned by these functions are printed to stdout by locked stub code in the editor.
+
+*/
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    area= length * width;
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter= 2*(length + width);
+    return perimeter;
+}
+
+
